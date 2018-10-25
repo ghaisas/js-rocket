9f1b61e569c8f4a6cb1336f9db07568ae742b3d7
ghaisas <isha1000@gmail.com>
2018-10-25 18:59:19 +0530

    added some more js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var scooter = {
+	make: 'access',
+	colour: 'white',
+	isTurnedOn: false,
+	seats: ['seat1','seat2'],
+
+},
+
+if (scooter.seats > 2) {
+	console.log ('your vehicle is not a scooter');
+} else {
+	console.log ('congrats, your vehicle is a scooter');
+}
+
+console.log('hello there friends!');
